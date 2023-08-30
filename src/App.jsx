c9e52a40b1e85801cebbf925c899d278623a3adf
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-08-30 18:18:48 +0100

    add styling and random number display

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 
+
+
+
 function App() {
   
+  function rand() {
+    return Math.floor(Math.random()*10)
+  }
+
+  let value1 = rand()
+  let value2 = rand()
+  let value3 = rand()
+  let value4 = rand()
+  let value5 = rand()
+  let value6 = rand()
+  let value7 = rand()
+  let value8 = rand()
+  let value9 = rand()
 
   return (
     <>
@@ -10,22 +26,20 @@ function App() {
        
         <h1>Number Blast</h1>
         <div>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>4</button>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>8</button>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>9</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value1}</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value2}</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value3}</button>
         </div>
         <div>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>1</button>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>2</button>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>5</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value4}</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value5}</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value6}</button>
         </div>
         <div>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>6</button>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>3</button>
-          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>7</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value7}</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value8}</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>{value9}</button>
         </div>
-        
-      
       </div>
     </>
   )
