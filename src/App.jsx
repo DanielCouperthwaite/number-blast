b30df5121f8745881035787654a17fb9f122add0
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-08-30 18:11:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       
+        <h1>Number Blast</h1>
+        <div>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>4</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>8</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>9</button>
+        </div>
+        <div>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>1</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>2</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>5</button>
+        </div>
+        <div>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>6</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>3</button>
+          <button style={{border: '2px solid blue', display: 'inline-block', padding: '20px', margin: '20px'}}>7</button>
+        </div>
+        
+      
+      </div>
+    </>
+  )
+}
+
+export default App
