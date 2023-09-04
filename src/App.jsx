f364e64ad2cc71dc0098ff51fb2f2e16cc771bca
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-09-04 13:28:54 +0100

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Game from './Game'
 import Home from './Home'
+import Leaderboard from './Leaderboard'
+import Instructions from './Instructions'
+import About from './About'
 
 
 
@@ -22,6 +25,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play" element={<Game />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
     </BrowserRouter>
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,12 @@ export default function Home () {
     return (
         <>
             <h1>Number Blast!</h1>
-            <Link className="link" style={{border: '5px solid black', fontSize: '2em', color:"black", margin: '10px', padding: "10px"}} to="/play">Play!   </Link>
+            <div>
+                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px"}} to="/play">Play!   </Link>
+                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px"}} to="/leaderboard">Leaderboard   </Link>
+                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px"}} to="/instructions">Instructions   </Link>
+                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px"}} to="/about">About  </Link>
+            </div>
         </>
     )
 }
