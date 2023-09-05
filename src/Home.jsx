ff178e9192cb63ca3adb4d3e20a06f7dc330f595
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-09-05 11:55:26 +0100

    home page link styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,10 +7,20 @@ export default function Home () {
         <>
             <h1>Number Blast!</h1>
             <div>
-                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em',  color:"black", margin: '10px', padding: "10px"}} to="/play">Play!   </Link>
-                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px"}} to="/leaderboard">Leaderboard   </Link>
-                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px"}} to="/instructions">Instructions   </Link>
-                <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px"}} to="/about">About  </Link>
+                <div>
+                    <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px", minWidth: '200px'}} to="/play">Play!   </Link>
+                </div>
+                <div>
+                    <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px", minWidth: '200px'}} to="/leaderboard">Leaderboard   </Link>
+                </div>
+                <div>
+                    <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px", minWidth: '200px'}} to="/instructions">Instructions   </Link>
+                </div>
+                <div>
+                    <Link className="link" style={{display: 'inline-block', border: '5px solid black', fontSize: '1.7em', color:"black", margin: '10px', padding: "10px", minWidth: '200px'}} to="/about">About  </Link>
+                </div>
+               
+            
             </div>
         </>
     )
